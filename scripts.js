c1eed16fc65315c31daff38042143466ccc188ef
Nicoam89 <nico.am98@gmail.com>
2025-06-18 08:27:43 -0300

    se agregan alerts

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// Division Equitativa
+// Division por Cantidad de Personas
 
   function calcular() {
     const monto = parseFloat(document.getElementById('monto').value) || 0;
@@ -8,9 +8,27 @@
     const resultadoDiv = document.getElementById('resultado');
     resultadoDiv.innerHTML = ""; // Limpia el resultado anterior
 
-    if (cantPersonas <= 0) {
-      resultadoDiv.textContent = "Por favor, ingresa una cantidad válida de personas.";
+    if (monto <= 0) {
+      alert("Por favor, ingresa un valor de ticket válido.");
       return;
+    }
+
+    if (cantPersonas <= 0) {
+      alert("Por favor, ingresa una cantidad válida de personas.");
+      return;
+    }
+
+    // Alerta si la propina es 0
+
+        if (propina === 0) {
+      alert("Recuerde dejar propina");
+    }
+
+        // Alerta si la propina es menor al 5% del ticket
+
+        const porcentajePropina = (propina / monto) * 100;
+    if (propina > 0 && porcentajePropina < 5) {
+      alert(`La propina es menor al 5% (${porcentajePropina.toFixed(2)}%). No seas rata.`);
     }
 
     const total = monto + propina;
@@ -21,3 +39,4 @@
       <h3 class"totales"><strong>Cada persona debe pagar:</strong> $${montoPorPersona}</h3>
     `;
   }
+
